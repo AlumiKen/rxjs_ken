33d309a3c59044f0a8fd449fe8c9503fd6f864bf
ken(呂冠宏) <ken@edetw.com>
2017-03-21 13:49:06 +0800

    ChangeDetectionStrategy

--- src/app/charts/flot/flot.component.ts
+++ src/app/charts/flot/flot.component.ts
@@ -1,16 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, NgZone, OnInit } from '@angular/core';
 import { FlotCharts } from './init';
+import 'rxjs';
 
 @Component({
   selector: 'app-flot',
   templateUrl: './flot.component.html',
-  styleUrls: ['./flot.component.css']
+  styleUrls: ['./flot.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FlotComponent implements OnInit {
 
-  constructor() { }
+  temp:number = 1;
+
+  constructor(private zone: NgZone, private cd:ChangeDetectorRef) {
+
+    Observable.interval(1000).subscribe((id) => {
+        /**
+         * 想要每秒+1呈現在畫面上
+         */
+        this.temp++;
+        /**
+         * 設定changeDetection: ChangeDetectionStrategy.OnPush
+         * 要加ChangeDetectorRef.markForCheck, view才會再更新
+         */
+        this.cd.markForCheck();
+      });
+  }
 
   ngOnInit() {
-    $(FlotCharts);
+    this.zone.runOutsideAngular(() => {
+      $(FlotCharts);
+    })
+  }
+
+  showText(){
+    console.log(new Date());
+    return 'haha';
   }
 }
